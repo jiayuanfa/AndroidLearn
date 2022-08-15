c8f86cfd4661541ae146a7d3de898c89339aa41a
贾元发 <jiayuanfa@yeah.net>
2022-08-15 18:55:39 +0800

    Kotlin环境搭建

---
+++ app/src/main/java/com/example/androidlearn/MainKotlinActivity.kt
@@ -0,0 +1,12 @@
+package com.example.androidlearn
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainKotlinActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+    }
+}
