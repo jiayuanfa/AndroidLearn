27343d0a2658dc4a35e0f88406ac4fd0bd4c90ed
贾元发 <jiayuanfa@yeah.net>
2022-08-16 14:37:23 +0800

    倒计时Kotlin版本

--- app/src/main/java/com/example/androidlearn/MainKotlinActivity.kt
+++ app/src/main/java/com/example/androidlearn/MainKotlinActivity.kt
@@ -1,12 +1,45 @@
 package com.example.androidlearn
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlin.concurrent.thread
 
+/**
+ * 倒计时Kotlin版本
+ */
 class MainKotlinActivity: AppCompatActivity() {
+
+    private val mHandler: Handler = object : Handler(Looper.getMainLooper()) {
+        override fun handleMessage(msg: Message) {
+            super.handleMessage(msg)
+
+            val timeTv = findViewById<TextView>(R.id.mCounterTv)
+            timeTv.text = msg.obj.toString()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
+
+        setContentView(R.layout.activity_main)
+
+        createThread()
+    }
+
+    private fun createThread() {
+        thread {
+            var time = 30
+            while (time >= 0) {
+                Thread.sleep(1000)
+                val message = Message()
+                message.obj = time.toString()
+                mHandler.sendMessage(message)
+                time--
+            }
+        }
     }
 }
